8b64d56567ed734cf947cf1adb2d70da4e4fd6f5
Siverena <siverena.ab@gmail.com>
2020-07-03 20:13:15 +0300

    точно все

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-var btnOpen = document.querySelector('.main-nav__open');
-var btnClose = document.querySelector('.main-nav__close');
-var menu = document.querySelector('.main-nav');
-
-
-btnOpen.addEventListener("click", function(e) {
-    menu.classList.toggle("main-nav__show");
-    btnOpen.classList.toggle("main-nav__open--op");
-})
